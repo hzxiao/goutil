219f75fbd636e5b6c6a989573c8f88ea10b1865a
Xiaozh <838223935@qq.com>
2017-07-13 16:31:03 +0800

    add assertion

---
+++ assert/assertion_test.go
@@ -0,0 +1,52 @@
+package assert
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestEqual(t *testing.T) {
+	Equal(t, "Hello", "Hello")
+
+	Equal(t, 123, 123)
+
+	Equal(t, 123.5, 123.5)
+
+	Equal(t, []byte("Hello World"), []byte("Hello World"))
+
+	Equal(t, nil, nil)
+
+	Equal(t, int32(123), int32(123))
+
+	Equal(t, uint64(123), uint64(123))
+
+	Equal(t, &struct{}{}, &struct{}{})
+}
+
+func TestNil(t *testing.T) {
+	Nil(t, nil)
+
+	Nil(t, (*struct{})(nil))
+}
+
+func TestTrue(t *testing.T) {
+	True(t, true)
+}
+
+func TestFalse(t *testing.T) {
+	False(t, false)
+}
+
+func TestNoError(t *testing.T) {
+	var err error
+
+	True(t, NoError(t, err))
+}
+
+func TestError(t *testing.T) {
+	var err error
+
+	err = errors.New("some err")
+
+	True(t, Error(t, err))
+}
