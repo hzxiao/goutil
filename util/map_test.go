21efe58df8cadd48be832ceae1269b788d44e193
Xiaozh <838223935@qq.com>
2018-02-22 22:53:38 +0800

    add util and map

---
+++ util/map_test.go
@@ -0,0 +1,109 @@
+package util
+
+import (
+	"github.com/hzxiao/goutil/assert"
+	"testing"
+)
+
+func TestMap_Get(t *testing.T) {
+	m := Map{}
+
+	m.Set("string", "1")
+	stringInterf := m.Get("string")
+	_string, ok := stringInterf.(string)
+	assert.True(t, ok)
+	assert.Equal(t, "1", _string)
+
+	m.Set("true", true)
+	m.Set("false", false)
+	assert.True(t, m.GetBool("true"))
+	assert.False(t, m.GetBool("false"))
+
+	m.Set("int", int(123))
+	assert.Equal(t, int64(123), m.GetInt64("int"))
+
+	m.Set("int8", int8(12))
+	assert.Equal(t, int64(12), m.GetInt64("int8"))
+
+	m.Set("int16", int16(123))
+	assert.Equal(t, int64(123), m.GetInt64("int16"))
+
+	m.Set("int32", int32(123))
+	assert.Equal(t, int64(123), m.GetInt64("int32"))
+
+	m.Set("int64", int64(123))
+	assert.Equal(t, int64(123), m.GetInt64("int64"))
+
+	m.Set("uint", uint(123))
+	assert.Equal(t, int64(123), m.GetInt64("uint"))
+
+	m.Set("uint8", uint8(123))
+	assert.Equal(t, int64(123), m.GetInt64("uint8"))
+
+	m.Set("uint16", uint16(123))
+	assert.Equal(t, int64(123), m.GetInt64("uint16"))
+
+	m.Set("uint32", uint32(123))
+	assert.Equal(t, int64(123), m.GetInt64("uint32"))
+
+	m.Set("uint64", uint64(123))
+	assert.Equal(t, int64(123), m.GetInt64("uint64"))
+
+	m.Set("float32", float32(123))
+	assert.Equal(t, float64(123), m.GetFloat64("float32"))
+
+	m.Set("float64", float64(123))
+	assert.Equal(t, float64(123), m.GetFloat64("float64"))
+
+	m.Set("map", Map{"int": 123})
+	assert.Len(t, m.GetMap("map"), 1)
+
+	m.Set("int64Array", []int64{1, 2, 3})
+	int64Array := m.GetInt64Array("int64Array")
+	assert.Len(t, int64Array, 3)
+
+	m.Set("stringArray", []string{"1", "2", "3"})
+	assert.Len(t, m.GetStringArray("stringArray"), 3)
+
+	m.Set("float64Array", []float64{1, 2, 3})
+	assert.Len(t, m.GetFloat64Array("float64Array"), 3)
+	m.Set("mapArray", []Map{
+		{"int": 123},
+		{"int": 123},
+		{"int": 123},
+	})
+	assert.Len(t, m.GetMapArray("mapArray"), 3)
+
+	assert.Len(t, m, 20)
+}
+
+func TestMap_GetP(t *testing.T) {
+	m := Map{}
+	m.Set("map", Map{
+		"int":         123,
+		"true":        true,
+		"float64":     float64(123),
+		"string":      "123",
+		"stringArray": []string{"1", "2"},
+		"map":         Map{"int": 123},
+		"mapArray": []Map{
+			Map{"int": 123},
+			Map{"int": 123},
+		},
+		"intArray":     []int64{1, 2, 3},
+		"float64Array": []float64{1, 2},
+	})
+
+	assert.Equal(t, int64(123), m.GetInt64P("map/int"))
+	assert.True(t, m.GetBoolP("map/true"))
+	assert.Equal(t, float64(123), m.GetFloat64P("map/float64"))
+	assert.Equal(t, "123", m.GetStringP("map/string"))
+	assert.Len(t, m.GetStringArrayP("map/stringArray"), 2)
+	assert.Equal(t, "2", m.GetStringP("map/stringArray/1"))
+	assert.Len(t, m.GetMapP("map/map"), 1)
+	assert.Equal(t, int64(123), m.GetInt64P("map/map/int"))
+	assert.Len(t, m.GetMapArrayP("map/mapArray"), 2)
+	assert.Equal(t, int64(123), m.GetInt64P("map/mapArray/0/int"))
+	assert.Len(t, m.GetInt64ArrayP("map/intArray"), 3)
+	assert.Len(t, m.GetFloat64ArrayP("map/float64Array"), 2)
+}
---
+++ util/util_test.go
@@ -0,0 +1,165 @@
+package util
+
+import (
+	"github.com/hzxiao/goutil/assert"
+	"testing"
+	"time"
+)
+
+type TestStruct struct {
+	Name string
+	Age  int
+}
+
+func TestStruct2Map(t *testing.T) {
+	//1
+	t1 := TestStruct{Name: "name", Age: 2}
+	m1 := Struct2Map(t1)
+	if m1["Name"].(string) != t1.Name || m1["Age"].(int) != t1.Age {
+		t.Error("error")
+	}
+
+	//2 struct pointer
+	t2 := &t1
+	m2 := Struct2Map(t2)
+	if m2["Name"].(string) != t2.Name || m2["Age"].(int) != t2.Age {
+		t.Error("error")
+	}
+
+	//3 string
+	m3 := Struct2Map("string")
+	if m3 != nil {
+		t.Error("error")
+	}
+}
+
+func BenchmarkStruct2Map(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Struct2Map(TestStruct{Name: "123", Age: 20})
+	}
+}
+
+func TestStruct2Json(t *testing.T) {
+	//1
+	t1 := &TestStruct{Name: "t1", Age: 2}
+	json1 := Struct2Json(t1)
+	if json1 == "" || json1 == "null" {
+		t.Error("struct point -> json error")
+	}
+	//2
+	t2 := TestStruct{Name: "t2", Age: 2}
+	json2 := Struct2Json(t2)
+	if json2 == "" || json2 == "null" {
+		t.Error("struct -> json error")
+	}
+}
+
+func TestBoolE(t *testing.T) {
+	v1 := true
+	b1, err := BoolE(v1)
+	assert.NoError(t, err)
+	assert.True(t, b1)
+
+	v2 := false
+	b2, err := BoolE(v2)
+	assert.NoError(t, err)
+	assert.False(t, b2)
+
+	//test error
+	_, err = BoolE(nil)
+	assert.Error(t, err)
+
+	_, err = BoolE(123)
+	assert.Error(t, err)
+}
+
+func TestBool(t *testing.T) {
+	assert.True(t, Bool(true))
+	assert.False(t, Bool(false))
+	assert.False(t, Bool(123))
+}
+
+func TestInt64E(t *testing.T) {
+	//time
+	_time, err := Int64E(time.Now())
+	assert.NoError(t, err)
+	assert.NotEqual(t, 0, _time)
+
+	//int
+	_int, err := Int64E(int(123))
+	assert.NoError(t, err)
+	assert.Equal(t, int64(123), _int)
+
+	//int8
+	_int8, err := Int64E(int8(12))
+	assert.NoError(t, err)
+	assert.Equal(t, int64(12), _int8)
+
+	//int16
+	_int16, err := Int64E(int16(123))
+	assert.NoError(t, err)
+	assert.Equal(t, int64(123), _int16)
+
+	//int32
+	_int32, err := Int64E(int32(123))
+	assert.NoError(t, err)
+	assert.Equal(t, int64(123), _int32)
+
+	//int64
+	_int64, err := Int64E(int64(123))
+	assert.NoError(t, err)
+	assert.Equal(t, int64(123), _int64)
+
+	//uint
+	_uint, err := Int64E(uint(123))
+	assert.NoError(t, err)
+	assert.Equal(t, int64(123), _uint)
+
+	//uint8
+	_uint8, err := Int64E(uint8(123))
+	assert.NoError(t, err)
+	assert.Equal(t, int64(123), _uint8)
+
+	//uint16
+	_uint16, err := Int64E(uint16(123))
+	assert.NoError(t, err)
+	assert.Equal(t, int64(123), _uint16)
+
+	//uint32
+	_uint32, err := Int64E(uint32(123))
+	assert.NoError(t, err)
+	assert.Equal(t, int64(123), _uint32)
+
+	//uint64
+	_uint64, err := Int64E(uint64(123))
+	assert.NoError(t, err)
+	assert.Equal(t, int64(123), _uint64)
+
+	//float32
+	_float32, err := Int64E(float32(123))
+	assert.NoError(t, err)
+	assert.Equal(t, int64(123), _float32)
+
+	//float64
+	_float64, err := Int64E(float64(123))
+	assert.NoError(t, err)
+	assert.Equal(t, int64(123), _float64)
+
+	//string
+	_string, err := Int64E("123")
+	assert.NoError(t, err)
+	assert.Equal(t, int64(123), _string)
+
+	//test error
+	_, err = Int64E(nil)
+	assert.Error(t, err)
+
+	_, err = Int64E(true)
+	assert.Error(t, err)
+
+	_, err = Int64E(TestStruct{})
+	assert.Error(t, err)
+
+	_, err = Int64E("aa")
+	assert.Error(t, err)
+}
